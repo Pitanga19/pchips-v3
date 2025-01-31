4ba305b205841ed4d5ced54dd39fc4ded4b55a64
Pitanga <lcmaciel99@gmail.com>
2025-01-31 17:34:08 -0300

    Add morgan logger, routes, web sockets. Use static files

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,39 @@
+// pchips-v3/server/index.ts
+
 import express, { Request, Response } from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import logger from 'morgan';
 
-const PORT = parseInt(process.env.PORT || '3000');
+const PORT: number = parseInt(process.env.PORT || '3000');
 
-const app = express();
+// ---------------- MAIN INIT   -------------------------------- //
+const app = express();              // Init express app
+const server = createServer(app);   // Create http server
+const io = new Server(server);      // Create web socket server
 
-// ROUTES
+// ---------------- MIDDLEWARES -------------------------------- //
+app.use(logger('dev'));             // Dev morgan logger
+app.use(express.static('client'));  // Use statics files
+app.use(express.json());            // Require & response json
+
+// ---------------- ROUTES  ------------------------------------ //
+// Simple main page using static files
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('<h1>Poker Chips app</h1>');
+    res.status(200).sendFile(process.cwd() + '/client/index.html');
 });
 
-// SERVER
-app.listen(PORT, () => {
+// ---------------- SOCKETS ------------------------------------ //
+// Simple connection socket
+io.on('connection', (socket) => {
+    console.log('A user has connected');
+    
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected');
+    });
+});
+
+// ---------------- SERVER  ------------------------------------ //
+server.listen(PORT, () => { 
     console.log(`Server is running on http://localhost:${PORT}`);
 });
