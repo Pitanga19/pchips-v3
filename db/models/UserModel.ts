c5cb6138838b6f598f4384254ad6e7be50ca7ae8
Pitanga <lcmaciel99@gmail.com>
2025-02-03 23:09:30 -0300

    Create User model

---
+++ db/models/UserModel.ts
@@ -0,0 +1,71 @@
+// pchips-v3/db/models/UserModel.ts
+
+import { Model, DataTypes } from 'sequelize';
+import { IUser } from './interfaces';
+import bcrypt from 'bcrypt';
+import sequelize from "../database";
+
+class UserModel extends Model {
+    public id!: number;
+    public username!: string;
+    public email!: string;
+    public password!: string;
+
+    createdAt!: Date;
+    updatedAt!: Date;
+
+    public toJSON(): IUser {
+        return {
+            id: this.id,
+            username: this.username,
+            email: this.email,
+        };
+    };
+
+    // Compare hashed password
+    public async comparePassword(password: string): Promise<boolean> {
+        return await bcrypt.compare(password, this.password);
+    };
+};
+
+UserModel.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        sequelize,
+        modelName: 'UserModel',
+        tableName: 'users',
+        timestamps: true,
+        hooks: {
+            // Hash password on create, on update
+            beforeCreate: async (user: UserModel): Promise<void> => {
+                user.password = await bcrypt.hash(user.password, 10);
+            },
+            beforeUpdate: async (user: UserModel): Promise<void> => {
+                if (user.changed('password')) {
+                    user.password = await bcrypt.hash(user.password, 10);
+                };
+            },
+        },
+    },
+);
+
+export default UserModel;
