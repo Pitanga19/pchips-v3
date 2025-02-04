2023dd1fb74523cc189b2d144b8cd4c815c7ba05
Pitanga <lcmaciel99@gmail.com>
2025-02-04 00:56:50 -0300

    Fix grammar

--- db/initDatabase.ts
+++ db/initDatabase.ts
@@ -7,12 +7,12 @@ const initDatabase = async () => {
     // ---------------- SYNC DATABASE   ------------------------ //
     try {
         await sequelize.authenticate(); // Connect to database
-        console.log('Database connection established succesfully');
+        console.log('Database connection established successfully');
 
         await sequelize.sync({          // Synchronize database
             force: true,    // To reset db while dev
         });
-        console.log('Database synchronized succesfully');
+        console.log('Database synchronized successfully');
     } catch (error) {
         console.error(`Error while initializing the database: ${error}\n`);
         throw error;
