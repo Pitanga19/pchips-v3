8c4f367a204af9c559e824d03ae6e1090b967cbe
Pitanga <lcmaciel99@gmail.com>
2025-02-22 13:31:53 -0300

    Update relation types

--- src/utils/types/relationTypes.ts
+++ src/utils/types/relationTypes.ts
@@ -5,8 +5,16 @@ import { TErrorList } from "./errorTypes";
 import UserModel from "../../../db/models/UserModel";
 import FriendModel from "../../../db/models/FriendModel";
 import BlockModel from "../../../db/models/BlockModel";
+import { IUser, IFriend, IBlock } from "../../../db/models/utils/interfaces";
+
+export type TUserModelList = UserModel[];
+export type TUserList = IUser[];
 
 export type TFriendModelReturn = FriendModel | null;
+export type TFriendReturn = IFriend | null;
+
+export type TBlockModelReturn = BlockModel | null;
+export type TBlockReturn = IBlock | null;
 
 export type TFriendServiceReturn = {
     status: EResponseStatus,
@@ -17,19 +25,10 @@ export type TFriendServiceReturn = {
 
 export type TFriendModelListReturn = {
     status: EResponseStatus,
-    friendModelList: UserModel[],
+    friendModelList: TUserModelList,
     errors: TErrorList,
     message: EResponseMessage,
 };
-
-export type TRelationDeleteReturn = {
-    status: EResponseStatus,
-    value: boolean,
-    errors: TErrorList,
-    message: EResponseMessage,
-};
-
-export type TBlockModelReturn = BlockModel | null;
 
 export type TBlockServiceReturn = {
     status: EResponseStatus,
@@ -40,7 +39,42 @@ export type TBlockServiceReturn = {
 
 export type TBlockModelListReturn = {
     status: EResponseStatus,
-    blockedModelList: UserModel[],
+    blockedModelList: TUserModelList,
+    errors: TErrorList,
+    message: EResponseMessage,
+};
+
+export type TRelationFriendReturn = {
+    status: EResponseStatus,
+    friend: TFriendReturn,
+    errors: TErrorList,
+    message: EResponseMessage,
+};
+
+export type TRelationFriendListReturn = {
+    status: EResponseStatus,
+    friendList: TUserList,
+    errors: TErrorList,
+    message: EResponseMessage,
+};
+
+export type TRelationBlockReturn = {
+    status: EResponseStatus,
+    block: TBlockReturn,
+    errors: TErrorList,
+    message: EResponseMessage,
+};
+
+export type TRelationBlockListReturn = {
+    status: EResponseStatus,
+    blockedList: TUserList,
+    errors: TErrorList,
+    message: EResponseMessage,
+};
+
+export type TRelationDeleteReturn = {
+    status: EResponseStatus,
+    value: boolean,
     errors: TErrorList,
     message: EResponseMessage,
 };
