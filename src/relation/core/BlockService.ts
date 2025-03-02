2b7eb78f68a3398601fa5aab8add54a7f05e7be8
Pitanga <lcmaciel99@gmail.com>
2025-03-02 16:45:18 -0300

    Update BlockService

--- src/relation/core/BlockService.ts
+++ src/relation/core/BlockService.ts
@@ -1,102 +1,116 @@
 // pchips-v3/src/relation/core/BlockService.ts
 
 import {
-    TBlockModelListReturn, TBlockModelReturn, TBlockServiceReturn, TRelationDeleteReturn,
+    TBlockData, TBlockFindData, TBlockFindResult, TBlockList, TBlockModel, TBlockService, TRelationDelete,
 } from '../relationIndex';
 import {
-    addToResponseErrors, EResponseMessage, EResponseStatus, TErrorList, EErrorField, EErrorMessage,
+    addToResponseErrors, TErrorList, EErrorField, EErrorMessage, showLog,
 } from '../../common/commonIndex';
 import { UserModel, BlockModel } from '../../../db/dbIndex';
+import { TUserDataList, TUserModelList, UserService } from '../../auth/authIndex';
+
+const file = 'BlockService';
+const field = EErrorField.BLOCK
 
 class BlockService {
-    private static async find(blockerId: number, blockedId: number, errors: TErrorList): Promise<TBlockModelReturn> {
-        const field = EErrorField.RELATION;
-        let blockModel: TBlockModelReturn = null;
+    private static async findBlock(errors: TErrorList, data: TBlockFindData): Promise<TBlockFindResult> {
+        const { blockerId, blockedId } = data;
+        let blockModel: TBlockModel = null;
+        let blockData: TBlockData = null;
 
         if (blockerId === blockedId) {
-            console.log(`[BlockService] Is same ID: ${blockerId}`);
-            addToResponseErrors(errors, field, EErrorMessage.SAME_USER);
+                    showLog(file, 'Is same ID', data, false);
+                    addToResponseErrors(errors, field, EErrorMessage.SAME_USER);
         } else {
-            blockModel = await BlockModel.findOne({ where: { blockerId, blockedId } });
+            blockModel = await BlockModel.findOne({ where: { blockerId, blockedId }});
+            if (blockModel) blockData = blockModel.toJSON();
         };
 
-        return blockModel;
+        return { blockModel, blockData };
+    };
+    
+    // Validate find result
+    private static validateFindResult(errors: TErrorList, data: TBlockFindData, findResult: TBlockFindResult, shouldExists: boolean): void {
+        if (shouldExists && !findResult.blockModel) {
+            showLog(file, 'Block not found', data, false);
+            addToResponseErrors(errors, field, EErrorMessage.NOT_FOUND);
+        } else if (!shouldExists && findResult.blockModel) {
+            showLog(file, 'Block already exist', data, false);
+            addToResponseErrors(errors, field, EErrorMessage.ALREADY_EXIST);
+        };
     };
 
-    public static async create(blockerId: number, blockedId: number): Promise<TBlockServiceReturn> {
-        const errors: TErrorList = [];
-        const field = EErrorField.RELATION;
-        let status: EResponseStatus = EResponseStatus.CREATED;
-        let message: EResponseMessage = EResponseMessage.CREATED;
-        let blockModel: TBlockModelReturn = await this.find(blockerId, blockedId, errors);
+    private static async find(errors: TErrorList, data: TBlockFindData, shouldExists: boolean): Promise<TBlockService> {
+        const findResult = await this.findBlock(errors, data);
+        this.validateFindResult(errors, data, findResult, shouldExists);
+        return findResult;
+    };
+
+    public static async create(errors: TErrorList, blockerId: number, blockedId: number): Promise<TBlockService> {
+        const findData = { blockerId, blockedId };
+        const findResult = await this.find(errors, findData, false);
+        let blockModel: TBlockModel = null;
+        let blockData: TBlockData = null;
 
         if (errors.length === 0) {
             blockModel = await BlockModel.create({ blockerId, blockedId });
+
+            if (!blockModel) {
+                showLog(file, 'Error creating block', { blockerId, blockedId }, false);
+                addToResponseErrors(errors, field, EErrorMessage.INTERNAL_SERVER_ERROR);
+            } else {
+                blockData = blockModel.toJSON();
+                showLog(file, 'Block successfully created', { blockerId, blockedId }, true);
+            };
         };
 
-        if (!blockModel) {
-            status = EResponseStatus.INTERNAL_SERVER_ERROR;
-            message = EResponseMessage.INTERNAL_SERVER_ERROR;
-            console.log(`[BlockService] Error creating Block: ${blockerId} - ${blockedId}`);
-            addToResponseErrors(errors, field, EErrorMessage.INTERNAL_SERVER_ERROR);
-        };
-
-        return { status, blockModel, errors, message };
+        return { blockModel, blockData };
     };
 
-    public static async get(blockerId: number, blockedId: number): Promise<TBlockServiceReturn> {
-        let status: EResponseStatus = EResponseStatus.SUCCESS;
-        let message: EResponseMessage = EResponseMessage.SUCCESS;
-        const errors: TErrorList = [];
-        const field = EErrorField.RELATION;
-        const blockModel = await this.find(blockerId, blockedId, errors);
+    public static async get(errors: TErrorList, blockerId: number, blockedId: number): Promise<TBlockService> {
+        const findData = { blockerId, blockedId };
+        const { blockModel, blockData } = await this.find(errors, findData, true);
 
-        if (!blockerId) {
-            status = EResponseStatus.NOT_FOUND;
-            message = EResponseMessage.NOT_FOUND;
-            console.log(`[BlockService] Block not found: ${blockerId} - ${blockedId}`);
-            addToResponseErrors(errors, field, EErrorMessage.NOT_FOUND);
-        };
-
-        return { status, blockModel, errors, message };
+        return { blockModel, blockData };
     };
 
-    public static async getBlockedModelList(blockerId: number): Promise<TBlockModelListReturn>{
-        const errors: TErrorList = [];
-        let status: EResponseStatus = EResponseStatus.SUCCESS;
-        let message: EResponseMessage = EResponseMessage.SUCCESS;
-        let blockedModelList: UserModel[] = [];
+    public static async getBlockedModelList(errors: TErrorList, userId: number): Promise<TBlockList>{
+        const { userModel, userData } = await UserService.getById(errors, userId);
+        let blockedModelList: TUserModelList = [];
+        let blockedDataList: TUserDataList = [];
 
-        const blockList = await BlockModel.findAll({
-            where: { blockerId },
+        const blockedList = await BlockModel.findAll({
+            where: { blockerId: userId },
             include: [
                 { model: UserModel, as: 'blocked', required: false },
             ],
         });
 
-        blockedModelList = blockList.map(b => 
+        blockedModelList = blockedList.map(b => 
             b.dataValues.blocked
         ).filter(Boolean);
+        blockedDataList = blockedModelList.map(b => b.toJSON());
 
-        console.log('[BlockService] Blocked users succesfully loaded\n', { status, blockedModelList: blockedModelList.map(b => b.toJSON()), errors, message });
+        showLog(file, 'Blockeds successfully loaded', blockedDataList, true);
 
-        return { status, blockedModelList, errors, message };
+        return { userModel, userData, blockedModelList, blockedDataList };
     };
 
-    public static async delete(blockerId: number, blockedId: number): Promise<TRelationDeleteReturn> {
-        let getBlockResult: TBlockServiceReturn = await this.get(blockerId, blockedId);
-        let status: EResponseStatus = getBlockResult.status;
-        let blockModel: TBlockModelReturn = getBlockResult.blockModel;
-        let errors: TErrorList = getBlockResult.errors;
-        let message: EResponseMessage = getBlockResult.message;
-        let value: boolean = false;
+    public static async delete(errors: TErrorList, blockerId: number, blockedId: number): Promise<TRelationDelete> {
+        const getBlockResult = await this.get(errors, blockerId, blockedId);
+        const { blockModel } = getBlockResult;
+        let { blockData } = getBlockResult;
+        let deleted: boolean = false;
 
-        if (errors.length === 0 && blockModel) {
+        if (errors.length === 0 && blockModel && blockData) {
+            showLog(file, 'Block successfully loaded', blockData, true);
+
             await blockModel.destroy();
-            value = true;
+            deleted = true;
+            showLog(file, 'Block successfully deleted.', { blockedId, blockerId }, true);
         };
 
-        return { status, value, errors, message };
+        return { deleted };
     };
 };
 
