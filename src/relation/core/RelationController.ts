0fcbf455a58a48b4886503d270ed38a2f41063f7
Pitanga <lcmaciel99@gmail.com>
2025-03-02 16:45:42 -0300

    Update RelationController

--- src/relation/core/RelationController.ts
+++ src/relation/core/RelationController.ts
@@ -2,126 +2,156 @@
 
 import { Request, Response } from 'express';
 import { RelationService, TRelationBlockBody, TRelationFriendBody, TRelationListBody } from '../relationIndex';
-import { EResponseMessage } from '../../common/commonIndex';
+import { EResponseStatus, handleResponseStatus, TErrorList } from '../../common/commonIndex';
 
 class RelationController {
     public static async sendFriendRequest(req: Request, res: Response): Promise<void> {
         const { senderId, receiverId }: TRelationFriendBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, friend, errors, message } = await RelationService.sendFriendRequest(senderId, receiverId);
+            const { friendModel, friendData } = await RelationService.sendFriendRequest(errors, senderId, receiverId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ friend, errors, message });
+            res.status(status).json({ friendModel, friendData, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
     
     public static async cancelFriendRequest(req: Request, res: Response): Promise<void> {
         const { senderId, receiverId }: TRelationFriendBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, value, errors, message } = await RelationService.cancelFriendRequest(senderId, receiverId);
+            const { deleted } = await RelationService.cancelFriendRequest(errors, senderId, receiverId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ value, errors, message });
+            res.status(status).json({ deleted, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async acceptFriendRequest(req: Request, res: Response): Promise<void> {
         const { senderId, receiverId }: TRelationFriendBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, friend, errors, message } = await RelationService.acceptFriendRequest(senderId, receiverId);
+            const { friendModel, friendData } = await RelationService.acceptFriendRequest(errors, senderId, receiverId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ friend, errors, message });
+            res.status(status).json({ friendModel, friendData, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async rejectFriendRequest(req: Request, res: Response): Promise<void> {
         const { senderId, receiverId }: TRelationFriendBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, value, errors, message } = await RelationService.rejectFriendRequest(senderId, receiverId);
+            const { deleted } = await RelationService.rejectFriendRequest(errors, senderId, receiverId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ value, errors, message });
+            res.status(status).json({ deleted, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async removeFriend(req: Request, res: Response): Promise<void> {
         const { senderId, receiverId }: TRelationFriendBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, value, errors, message } = await RelationService.removeFriend(senderId, receiverId);
+            const { deleted } = await RelationService.removeFriend(errors, senderId, receiverId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ value, errors, message });
+            res.status(status).json({ deleted, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async blockUser(req: Request, res: Response): Promise<void> {
         const { blockerId, blockedId }: TRelationBlockBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, block, errors, message } = await RelationService.blockUser(blockerId, blockedId);
+            const { blockModel, blockData } = await RelationService.blockUser(errors, blockerId, blockedId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ block, errors, message });
+            res.status(status).json({ blockModel, blockData, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async unblockUser(req: Request, res: Response): Promise<void> {
         const { blockerId, blockedId }: TRelationBlockBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, value, errors, message } = await RelationService.unblockUser(blockerId, blockedId);
+            const { deleted } = await RelationService.unblockUser(errors, blockerId, blockedId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ value, errors, message });
+            res.status(status).json({ deleted, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getAcceptedFriendList(req: Request, res: Response): Promise<void> {
         const { userId }: TRelationListBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, friendList, errors, message } = await RelationService.getAcceptedFriendList(userId);
+            const { userModel, userData, friendModelList, friendDataList } = await RelationService.getAcceptedFriendList(errors, userId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ friendList, errors, message });
+            res.status(status).json({ userModel, userData, friendModelList, friendDataList, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getPendingFriendList(req: Request, res: Response): Promise<void> {
         const { userId }: TRelationListBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, friendList, errors, message } = await RelationService.getPendingFriendList(userId);
+            const { userModel, userData, friendModelList, friendDataList } = await RelationService.getPendingFriendList(errors, userId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ friendList, errors, message });
+            res.status(status).json({ userModel, userData, friendModelList, friendDataList, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getBlockedList(req: Request, res: Response): Promise<void> {
         const { userId }: TRelationListBody = req.body;
+        const expectedOk = EResponseStatus.SUCCESS;
+        const errors: TErrorList = [];
 
         try {
-            const { status, blockedList, errors, message } = await RelationService.getBlockedList(userId);
+            const { userModel, userData, blockedModelList, blockedDataList } = await RelationService.getBlockedList(errors, userId);
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ blockedList, errors, message });
+            res.status(status).json({ userModel, userData, blockedModelList, blockedDataList, errors });
         } catch (error){
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 };
