f64f4aa0dcfe8c8ab6acd10325842a29fe8fa8a4
Pitanga [Work-recep] <lcmaciel99@gmail.com>
2025-03-02 05:44:59 -0300

    Update AuthRoutes

--- src/auth/core/AuthRoutes.ts
+++ src/auth/core/AuthRoutes.ts
@@ -15,14 +15,19 @@ router.post('/login', AuthMiddleware.validateLoginInputs, async (req: Request, r
     await AuthController.login(req, res);
 });
 
-// Recover forgoted password
-router.post('/recover-password', AuthMiddleware.validateRecoverInputs, async (req: Request, res: Response) => {
-    await AuthController.recover(req, res);
-});
-
 // Update user info (username, email, password)
 router.put('/update-user', AuthMiddleware.validateUpdateInputs, async (req: Request, res: Response) => {
     await AuthController.update(req, res);
+});
+
+// Recover forgoted password
+router.post('/recover-password', AuthMiddleware.validateRecoverInputs, async (req: Request, res: Response) => {
+    await AuthController.recoverPassword(req, res);
+});
+
+// Delete account
+router.delete('/account', AuthMiddleware.validateDeleteInputs, async (req: Request, res: Response) => {
+    await AuthController.deleteAccount(req, res);
 });
 
 export default router;
