5f6aa0ebc4dee0445f3bd775db2ea7ea21488d09
Pitanga [Work-recep] <lcmaciel99@gmail.com>
2025-03-03 03:51:22 -0300

    Update settingTypes

--- src/setting/utils/settingTypes.ts
+++ src/setting/utils/settingTypes.ts
@@ -1,15 +1,13 @@
 // pchips-v3/src/setting/utils/settingTypes.ts
 
-import { EBlindIncreaseType, EBlindLevel, ERebuyAddon, EStartingChipsType, ETableSize, ISetting, SettingModel } from "../../../db/dbIndex";
-import { TErrorList } from "../../common/commonIndex";
+import { ISetting, SettingModel } from "../../../db/dbIndex";
 
-export type TSettingModelReturn = SettingModel | null;
+export type TSettingModel = SettingModel | null;
 export type TSettingData = ISetting | null;
 
-export type TSettingServiceReturn = {
-    settingModel: TSettingModelReturn;
+export type TSettingService = {
+    settingModel: TSettingModel;
     settingData: TSettingData;
-    errors: TErrorList;
 };
 
 export type TSettingUpdates = Omit<Partial<ISetting>, 'id' | 'gameId'>;
