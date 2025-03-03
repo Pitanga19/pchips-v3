aa56d9b1340056ea767fdd5ab318e298bc79da58
Pitanga [Work-recep] <lcmaciel99@gmail.com>
2025-03-03 03:51:32 -0300

    Update SettingService

--- src/setting/core/SettingService.ts
+++ src/setting/core/SettingService.ts
@@ -1,66 +1,63 @@
 // pchips-v3/src/setting/core/SettingService.ts
 
-import { TSettingData, TSettingModelReturn, TSettingServiceReturn, TSettingUpdates } from '../settingIndex';
+import { TSettingData, TSettingModel, TSettingService, TSettingUpdates } from '../settingIndex';
 import { addToResponseErrors, EErrorField, EErrorMessage, TErrorList } from '../../common/commonIndex';
 import { SettingModel } from '../../../db/dbIndex';
 
 class SettingService {
-    private static async find(gameId: number, errors: TErrorList, expectedExists: boolean): Promise<TSettingModelReturn> {
+    private static async find(errors: TErrorList, gameId: number, expectedExists: boolean): Promise<TSettingModel> {
         const settingModel = await SettingModel.findOne({ where: { gameId } });
         
         if (!settingModel && expectedExists) {
             console.log(`Setting not found: gameId: ${gameId}`);
             const field = EErrorField.SETTING;
-            const message = EErrorMessage.SETTING_NOT_FOUND;
+            const message = EErrorMessage.NOT_FOUND;
             addToResponseErrors(errors, field, message);
         } else if (settingModel && !expectedExists) {
             console.log(`Setting already exists: gameId: ${gameId}`);
             const field = EErrorField.SETTING;
-            const message = EErrorMessage.EXISTING_SETTING;
+            const message = EErrorMessage.ALREADY_EXIST;
             addToResponseErrors(errors, field, message);
         };
 
         return settingModel;
     };
 
-    public static async create(gameId: number): Promise<TSettingServiceReturn> {
-        const errors: TErrorList = [];
-        const expectedExists = false;
-        let settingModel: TSettingModelReturn = null;
+    public static async create(errors: TErrorList, gameId: number): Promise<TSettingService> {
+        let settingModel: TSettingModel = null;
         let settingData: TSettingData = null;
-
-        await this.find(gameId, errors, expectedExists);
+        
+        const expectedExists = false;
+        await this.find(errors, gameId, expectedExists);
 
         if (errors.length === 0) {
             settingModel = await SettingModel.create({ gameId });
             settingData =settingModel.toJSON();
         };
 
-        return { settingModel, settingData, errors };
+        return { settingModel, settingData };
     };
 
-    public static async get(gameId: number): Promise<TSettingServiceReturn> {
-        const errors: TErrorList = [];
+    public static async get(errors: TErrorList, gameId: number): Promise<TSettingService> {
         const expectedExists = true;
-        const settingModel = await this.find(gameId, errors, expectedExists);
+        const settingModel = await this.find(errors, gameId, expectedExists);
         const settingData: TSettingData = settingModel ? settingModel.toJSON() : null;
 
-        return { settingModel, settingData, errors };
+        return { settingModel, settingData };
     };
 
-    public static async update(gameId: number, updates: TSettingUpdates): Promise<TSettingServiceReturn> {
-        const errors: TErrorList = [];
+    public static async update(errors: TErrorList, gameId: number, updates: TSettingUpdates): Promise<TSettingService> {
         const expectedExists = true;
         let settingData: TSettingData = null;
 
-        const settingModel = await this.find(gameId, errors, expectedExists);
+        const settingModel = await this.find(errors, gameId, expectedExists);
 
         if (errors.length === 0 && settingModel) {
             await settingModel.update(updates);
             settingData = settingModel.toJSON();
         };
 
-        return { settingModel, settingData, errors };
+        return { settingModel, settingData };
     };
 };
 
