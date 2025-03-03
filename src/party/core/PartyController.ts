b15165c170d54b370f3c3935fd3978e50f1a8aee
Pitanga [Work-recep] <lcmaciel99@gmail.com>
2025-03-03 03:46:14 -0300

    Update PartyController

--- src/party/core/PartyController.ts
+++ src/party/core/PartyController.ts
@@ -2,176 +2,218 @@
 
 import { Request, Response } from 'express';
 import {
-    PartyManagementService, TPartyCreateBody, TPartyManageBody, TPartyUpdateBody, TPartyListBody, TPartyMembersBody,
+    PartyManagementService, TPartyCreateBody, TPartyManageBody, TPartyUpdateBody, TPartyMembersBody, TPartySelfBody, TUserPartiesBody,
 } from '../partyIndex';
-import { EResponseMessage } from '../../common/commonIndex';
+import { EResponseStatus, handleResponseStatus, TErrorList } from '../../common/commonIndex';
 
 class PartyController {
     public static async create(req: Request, res: Response): Promise<void> {
-        const { partyName, userId }:TPartyCreateBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyName }: TPartyCreateBody = req.body;
 
         try {
-            const { status, party, partyUser, errors, message } = await PartyManagementService.create(partyName, userId);
+            const { actorModel, actorData, partyModel, partyData, partyActorModel, partyActorData } = await PartyManagementService.create(errors, actorId, partyName);
+            const expectedOk = EResponseStatus.CREATED;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, partyUser, errors, message });
+            res.status(status).json({ actorModel, actorData, partyModel, partyData, partyActorModel, partyActorData, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async addUser(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId, targetId }:TPartyManageBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyId, targetId }: TPartyManageBody = req.body;
 
         try {
-            const { status, party, partyUser, errors, message } = await PartyManagementService.addUser(actorId, partyId, targetId);
+            const { actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData } = await PartyManagementService.addUser(errors, actorId, partyId, targetId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, partyUser, errors, message });
+            res.status(status).json({ actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async removeUser(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId, targetId }:TPartyManageBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyId, targetId }: TPartyManageBody = req.body;
 
         try {
-            const { status, party, value, errors, message } = await PartyManagementService.removeUser(actorId, partyId, targetId);
+            const { actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, deleted } = await PartyManagementService.removeUser(errors, actorId, partyId, targetId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, value, errors, message });
+            res.status(status).json({ actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, deleted, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async leave(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId }:TPartyManageBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyId }: TPartySelfBody = req.body;
 
         try {
-            const { status, party, value, errors, message } = await PartyManagementService.leave(actorId, partyId);
+            const { actorModel, actorData, partyModel, partyData, deleted } = await PartyManagementService.leave(errors, actorId, partyId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, value, errors, message });
+            res.status(status).json({ actorModel, actorData, partyModel, partyData, deleted, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async assignAdmin(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId, targetId }:TPartyManageBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyId, targetId }: TPartyManageBody = req.body;
 
         try {
-            const { status, party, partyUser, errors, message } = await PartyManagementService.assignAdmin(actorId, partyId, targetId);
+            const { actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData } = await PartyManagementService.assignAdmin(errors, actorId, partyId, targetId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, partyUser, errors, message });
+            res.status(status).json({ actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async removeAdmin(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId, targetId }:TPartyManageBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyId, targetId }: TPartyManageBody = req.body;
 
         try {
-            const { status, party, partyUser, errors, message } = await PartyManagementService.removeAdmin(actorId, partyId, targetId);
+            const { actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData } = await PartyManagementService.removeAdmin(errors, actorId, partyId, targetId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, partyUser, errors, message });
+            res.status(status).json({ actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async transferOwner(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId, targetId }:TPartyManageBody = req.body;
+        const errors: TErrorList = [];
+        const { actorId, partyId, targetId }: TPartyManageBody = req.body;
 
         try {
-            const { status, party, partyUser, errors, message } = await PartyManagementService.transferOwner(actorId, partyId, targetId);
+            const { actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData } = await PartyManagementService.transferOwner(errors, actorId, partyId, targetId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, partyUser, errors, message });
+            res.status(status).json({ actorModel, actorData, targetModel, targetData, partyModel, partyData, partyActorModel, partyActorData, partyTargetModel, partyTargetData, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
-    public static async rename(req: Request, res: Response): Promise<void> {
-        const { actorId, partyId, newName }:TPartyUpdateBody = req.body;
+    public static async update(req: Request, res: Response): Promise<void> {
+        const errors: TErrorList = [];
+        const { actorId, partyId, updates }: TPartyUpdateBody = req.body;
 
         try {
-            const { status, party, partyUser, errors, message } = await PartyManagementService.rename(actorId, partyId, newName);
+            const { actorModel, actorData, partyModel, partyData, partyActorModel, partyActorData } = await PartyManagementService.update(errors, actorId, partyId, updates);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, partyUser, errors, message });
+            res.status(status).json({ actorModel, actorData, partyModel, partyData, partyActorModel, partyActorData, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getUserAllParties(req: Request, res: Response): Promise<void> {
-        const { userId }:TPartyListBody = req.body;
+        const errors: TErrorList = [];
+        const { userId }: TUserPartiesBody = req.body;
 
         try {
-            const { status, user, partyList, errors, message } = await PartyManagementService.getUserAllParties(userId);
+            const { userModel, userData, partyModelList, partyDataList } = await PartyManagementService.getUserAllParties(errors, userId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ user, partyList, errors, message });
+            res.status(status).json({ userModel, userData, partyModelList, partyDataList, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getUserAdminParties(req: Request, res: Response): Promise<void> {
-        const { userId }:TPartyListBody = req.body;
+        const errors: TErrorList = [];
+        const { userId }: TUserPartiesBody = req.body;
 
         try {
-            const { status, user, partyList, errors, message } = await PartyManagementService.getUserAdminParties(userId);
+            const { userModel, userData, partyModelList, partyDataList } = await PartyManagementService.getUserAdminParties(errors, userId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ user, partyList, errors, message });
+            res.status(status).json({ userModel, userData, partyModelList, partyDataList, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getUserOwnerParties(req: Request, res: Response): Promise<void> {
-        const { userId }:TPartyListBody = req.body;
+        const errors: TErrorList = [];
+        const { userId }: TUserPartiesBody = req.body;
 
         try {
-            const { status, user, partyList, errors, message } = await PartyManagementService.getUserOwnerParties(userId);
+            const { userModel, userData, partyModelList, partyDataList } = await PartyManagementService.getUserOwnerParties(errors, userId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ user, partyList, errors, message });
+            res.status(status).json({ userModel, userData, partyModelList, partyDataList, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getPartyMembers(req: Request, res: Response): Promise<void> {
-        const { partyId }:TPartyMembersBody = req.body;
+        const errors: TErrorList = [];
+        const { partyId }: TPartyMembersBody = req.body;
 
         try {
-            const { status, party, userList, errors, message } = await PartyManagementService.getPartyMembers(partyId);
+            const { partyModel, partyData, userModelList, userDataList } = await PartyManagementService.getPartyMembers(errors, partyId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, userList, errors, message });
+            res.status(status).json({ partyModel, partyData, userModelList, userDataList, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getPartyAdmins(req: Request, res: Response): Promise<void> {
-        const { partyId }:TPartyMembersBody = req.body;
+        const errors: TErrorList = [];
+        const { partyId }: TPartyMembersBody = req.body;
 
         try {
-            const { status, party, userList, errors, message } = await PartyManagementService.getPartyAdmins(partyId);
+            const { partyModel, partyData, userModelList, userDataList } = await PartyManagementService.getPartyAdmins(errors, partyId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, userList, errors, message });
+            res.status(status).json({ partyModel, partyData, userModelList, userDataList, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 
     public static async getPartyOwners(req: Request, res: Response): Promise<void> {
-        const { partyId }:TPartyMembersBody = req.body;
+        const errors: TErrorList = [];
+        const { partyId }: TPartyMembersBody = req.body;
 
         try {
-            const { status, party, userList, errors, message } = await PartyManagementService.getPartyOwners(partyId);
+            const { partyModel, partyData, userModelList, userDataList } = await PartyManagementService.getPartyOwners(errors, partyId);
+            const expectedOk = EResponseStatus.SUCCESS;
+            const status = handleResponseStatus(errors, expectedOk);
 
-            res.status(status).json({ party, userList, errors, message });
+            res.status(status).json({ partyModel, partyData, userModelList, userDataList, errors });
         } catch (error) {
-            res.status(500).json({ error, message: EResponseMessage.INTERNAL_SERVER_ERROR });
+            res.status(500).json({ error });
         };
     };
 };
